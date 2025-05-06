4912f073f66fbc587bb820c089b952b2e7493bfd
jytole <kylerssmith@hotmail.com>
2025-05-06 13:50:33 -0500

    Init: create hardwareSimulator and add notes

---
+++ tools/bin/hardwareSimulator.py
@@ -0,0 +1,2 @@
+### Hardware Simulator logic: implement a way for each builtInChip to receive inputs from others / pass outputs to others
+### Then, process a .hdl file to implement these connections
