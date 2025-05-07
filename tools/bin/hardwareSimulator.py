ddf9e1a8e9c5df4259631b5a788e5b582b815ef2
jytole <kylerssmith@hotmail.com>
2025-05-07 12:38:29 -0500

    Begin implementing hardwareSimulator

--- tools/bin/hardwareSimulator.py
+++ tools/bin/hardwareSimulator.py
@@ -1,2 +1,24 @@
 ### Hardware Simulator logic: implement a way for each builtInChip to receive inputs from others / pass outputs to others
 ### Then, process a .hdl file to implement these connections
+
+### each "part" should contain a dictionary of pins mapped to wires
+### a verification function could make sure each pin of a built-in chip is labeled
+
+class Circuit():
+    def __init__(self):
+        self.inputs = {} # input pin names
+        self.outputs = {} # output pin names
+        self.internals = {} # internal wire names
+        self.parts = {} # parts with dictionaries of pins mapped to wires
+        
+def tst_parser(filepath):
+    with open(filepath, "r") as file:
+        for line in file:
+            if line.startswith("import"):
+                print("importing something")
+                
+def hdl_parser(filepath):
+    with open(filepath, "r") as file:
+        for line in file:
+            if line.startswith("import"):
+                print("importing something")
